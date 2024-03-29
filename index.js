7a89d4b5814dc7e751be739e258397d98e81e86f
Brandolt55 <gabrielbrandolt55@gmail.com>
2024-03-29 18:34:58 -0300

    index

--- index.js
+++ index.js
@@ -10,8 +10,18 @@ const authDocProduction = require('./src/middlewares/authDoc');
 const app = express();
 require('dotenv').config();
 
-// Configuração do CORS
-app.use(cors());
+// Permitindo acesso de origens específicas
+const allowedOrigins = ['https://backend-api-weld.vercel.app', 'http://localhost:3000']; // Adicione aqui os URLs de suas aplicações clientes
+app.use(cors({
+  origin: function (origin, callback) {
+    // Verificando se a origem está na lista de origens permitidas ou se não está definida (o que indica uma requisição de API)
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+}));
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -27,10 +37,9 @@ if (process.env.NODE_ENV !== 'test') {
 
 routes(app);
 
-
 if (process.env.NODE_ENV !== 'test') {
   const PORT = process.env.PORT || 4000;
-  app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`))
+  app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
 }
 
 module.exports = app;
